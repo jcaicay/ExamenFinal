9806e324883e6ba73bc5f5f0b5f63849ddbb13cb
jcaicay <jcaicay@gmail.com>
2024-12-06 22:05:54 -0500

    Cambios

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, abort, jso
 import sqlite3
 
 def get_db_connection():
-    conn = sqlite3.connect("database.db")
+    conn = sqlite3.connect("alumnos.db")
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -24,63 +24,63 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route("/post", methods=["GET"])
-def get_all_posts():
+@app.route("/alumno", methods=["GET"])
+def get_all_alumnos():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
+    alumnos = conn.execute('SELECT * FROM alumnos').fetchall()
     conn.close()
-    posts_lists = [dict(post) for post in posts]
-    return jsonify(posts_lists)
+    alumnos_lists = [dict(post) for post in alumnos]
+    return jsonify(alumnos_lists)
 
-@app.route("/post/<int:post_id>", methods=["GET"])
+@app.route("/alumno/<int:post_id>", methods=["GET"])
 def get_one_post(post_id: int):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    post = conn.execute('SELECT * FROM alumnos WHERE id = ?', (post_id,)).fetchone()
     conn.close()
     if post is None:
         abort(404, description="Post not found")
     return jsonify(dict(post))
 
 
-@app.route("/post/create", methods=["POST"])
+@app.route("/alumno/create", methods=["POST"])
 def create_one_post():
     data = request.json
     title = data.get('title')
     content = data.get('content')
     
     conn = get_db_connection()
-    conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+    conn.execute('INSERT INTO alumnos (title, content) VALUES (?, ?)', (title, content))
     conn.commit()
     conn.close()
-    return jsonify({"message": "Post created"})
+    return jsonify({"message": "Alumno created"})
 
 
-@app.route("/post/edit/<int:post_id>", methods=["PUT"])
+@app.route("/alumno/edit/<int:post_id>", methods=["PUT"])
 def edit_one_post(post_id):
     data = request.json
     title = data.get('title')
     content = data.get('content')
 
     conn = get_db_connection()
-    conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
+    conn.execute('UPDATE alumnos SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
     conn.commit()
     conn.close()
 
-    return jsonify({"message": "Post updated"})
+    return jsonify({"message": "Alumno updated"})
 
 
-@app.route("/post/delete/<int:post_id>", methods=["POST"])
+@app.route("/alumno/delete/<int:post_id>", methods=["POST"])
 def delete_one_post(post_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    post = conn.execute('SELECT * FROM alumnos WHERE id = ?', (post_id,)).fetchone()
     
     if post is None:
         abort(404)
 
-    conn.execute('DELETE FROM posts WHERE id = ?', (post_id,))
+    conn.execute('DELETE FROM alumnos WHERE id = ?', (post_id,))
     conn.commit()
     conn.close()
-    return redirect(url_for('get_all_posts'))
+    return redirect(url_for('get_all_alumnos'))
 
 
 # Iniciar el servidor
